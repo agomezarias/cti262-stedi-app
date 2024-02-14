494a3d19d51303009b702486b49c2843f133338f
agomezarias <alberto_gomez_arias@yahoo.com>
2024-02-13 18:36:16 -0600

    helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,8 +2,12 @@ const myName = () => {
     return "Alberto"
 }
 
+const hello = () => {
+    return "Hello"
+}
+
 const sayHello = (userName) => {
-    return "Hello " + userName();
+    return (hello() + " " + userName());
 }
 
 console.log(sayHello(myName));
